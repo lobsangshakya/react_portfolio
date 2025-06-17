6899a2bab0b4ca983b4cfde2f91460b2c80160ca
Lobsang Tsetan Shakya <lobsangshakya5@gmail.com>
2025-06-17 10:37:16 +0545

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd())
+  const env = loadEnv(mode, process.cwd());
 
   return {
     plugins: [react()],
-    base: env.VITE_BASE_PATH || "/react-portfolio",
-  }
-})
+    base: env.VITE_BASE_PATH || "/react_portfolio"
+  };
+});
