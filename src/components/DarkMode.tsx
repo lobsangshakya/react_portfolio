99a0220fef5adba9fff74d7ec70a67c0828da186
lobsangshakya <lobsangshakya5@gmail.com>
2025-12-13 20:15:50 +0530

    Cleaned and Updated UI/UX Interface

--- src/components/DarkMode.tsx
+++
@@ -1,9 +0,0 @@
-// import React from "react";
-import "./styles.css"
-interface Props {
-    children : string
-}
-
-export const DarkMode = ({ children } : Props) => {
-  return <div className="dark-mode">{children}</div>;
-};
